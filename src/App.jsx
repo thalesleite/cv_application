0b8c87bd4964f24e451b3c96bfb6d23f052b4e28
thalesleite <thalesaleite@gmail.com>
2024-04-16 17:09:21 +0100

    create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css"
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h1>CV Application</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
